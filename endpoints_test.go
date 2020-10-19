112888577fb2f98bcc0bb622f37be819d599746b
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-19 14:54:02 +0530

    Added unit test for GET /meetings

---
+++ endpoints_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http/httptest"
+	"net/http"
+	"testing"
+
+	"github.com/ramizkhan99/meetingsAPI/src/controllers"
+)
+
+func TestGetMeetings(t *testing.T) {
+	req, err := http.NewRequest("GET", "/meeetings", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.MeetingHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Unit test failed with: %v | %v", status, rr.Code)
+	}
+}
